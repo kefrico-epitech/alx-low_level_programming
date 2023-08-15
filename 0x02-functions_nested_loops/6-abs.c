80a496f706b6ea3b0db65ca5a856530f5e4750de
kefrico-epitech <124151797+kefrico-epitech@users.noreply.github.com>
2023-08-15 18:42:25 +0200

    hot fix

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,15 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * _abs - calculate the absolute value of the number from zero.
- * @i: the integer to be converted.
- * Return: absolute value of the input number.
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
  */
-int _abs(int i)
+int _abs(int n)
 {
-	if (i < 0)
-		i = i * -1;
-	return (i);
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
 }
